a6588687a675efeae7953fd6d989dc74c4ca6f92
Sriram <65888303+Sriram-2210@users.noreply.github.com>
2022-07-06 06:13:36 +0000

    Commit

--- Practice/list.py
+++ Practice/list.py
@@ -1,3 +1,21 @@
 car = ['honda', 'benz', 'bmw', 'ferrari']
+'''
+#Reading list data
 x = int(input('Enter value:'))
 print(car[x].upper())
+
+#Appending list data
+car.append('truck')
+print(car)
+
+#Insert list data
+car.insert(0,'bugatti')
+print(car)
+
+#pop operation
+car.pop()
+print(car)
+
+poped = car.pop(0)
+print('My first car is ' + poped.title())
+'''
